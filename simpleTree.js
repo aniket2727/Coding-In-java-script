22d7932f45559e3f04db9f782f105ccf0ae28b28
aniket2727 <aniketkadam5950@gmail.com>
2024-02-10 18:41:26 +0530

    added linked list

--- simpleTree.js
+++ simpleTree.js
@@ -1,88 +1,42 @@
-
-
-
-
-//  craeted simple tress  in javascript 
-class node{
-    constructor(data){
-        this.data=data;
-        this.left=null;
-        this.right=null;
+class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
     }
-
-}
-
-
-
-class bts_node{
-    constructor(){
-       this.root=null;
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.head = null;
     }
-    insert(data){
-
-
-        newnode=new node(data)
-
-        if(!this.root){
-            this.root=newnode;
-        }
-        else{
-            this.insertvalue(this.root,data)
-        }
+  
+    append(data) {
+      const newNode = new Node(data);
+      if (!this.head) {
+        this.head = newNode;
+        return;
+      }
+      let lastNode = this.head;
+      while (lastNode.next) {
+        lastNode = lastNode.next;
+      }
+      lastNode.next = newNode;
     }
-
-    insertvalue(node,newnode){
-
-        if(newnode.data<node.data){
-            if(node.left===null){
-                node.left=newnode
-            }
-            else{
-                this.insertvalue(node.left,newnode)
-            }
-        }
-
-
-        else{
-            if(node.right===null){
-                node.right=newnode;
-            }
-
-            else{
-                this.insertvalue(this.right,newnode)
-            }
-        }
-
+  
+    display() {
+      let current = this.head;
+      while (current) {
+        console.log(current.data + " -> ");
+        current = current.next;
+      }
+      console.log("null");
     }
-
-
-    inorder(node,callback){
-        if(node!=null){
-            this.inorder(node.left,callback);
-            callback(node.data);
-            this.inorder(node.right,callback);
-        }
-    }
-
-}
-
-
-
-
-const bst=new bts_node();
-
-bst.insert(10)
-bst.insert(5);
-bst.insert(15);
-bst.insert(3);
-bst.insert(7);
-bst.insert(12);
-bst.insert(18);
-
-const result=[]
-bst.inorderTraversal(bst.root, (data) => result.push(data));
-console.log(result)
-
-
-
-
+  }
+  
+  
+  const linkedList = new LinkedList();
+  linkedList.append(1);
+  linkedList.append(2);
+  linkedList.append(3);
+  linkedList.display();
+  
