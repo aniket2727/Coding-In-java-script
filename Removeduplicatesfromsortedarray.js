d646cd1f82ac2567db276110cfcb0fd5d7f4c0e6
aniket2727 <aniketkadam5950@gmail.com>
2024-02-11 14:21:48 +0530

    remove duplicates from the array

---
+++ Removeduplicatesfromsortedarray.js
@@ -0,0 +1,26 @@
+
+
+
+
+//remove duplicateds from sorted array  
+
+
+
+function removeDuplicates(data) {
+    if (data.length <= 1) {
+        return data;
+    }
+
+    let result = [data[0]];
+
+    for (let i = 1; i < data.length; i++) {
+        if (data[i] !== data[i - 1]) {
+            result.push(data[i]);
+        }
+    }
+
+    return result;
+}
+
+const result = removeDuplicates([1, 2, 2, 3, 3, 4, 5, 6]);
+console.log(result);
